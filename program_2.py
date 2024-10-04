8594d5499e85da6eb61c557a3d045cceccd8bb56
LizzieK549 <klizzy630@gmail.com>
2024-10-04 18:21:01 -0500

    Week 5 homework
    Lizzie Kerr, 10/4, Project 5

--- program_2.py
+++ program_2.py
@@ -1,9 +1,6 @@
 # Program #2: Math Quiz
-# Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
-
-#     247
-
-# + 129
+# Write a program that gives simple math quizzes.
+# The program should display two random numbers to be added, such as 247 + 129
 
 # ------
 
@@ -11,3 +8,24 @@
 # If the answer is correct, a message of congratulations should be displayed.  
 # If the answer is incorrect a message showing the correct answer should be displayed.  
 # The program must use a function that accomplishes part of the needed tasks.
+
+import random
+
+def mathProblem():
+    x = random.randint(0,1000) # googled random.randint to generate random numbers
+    y = random.randint(0,1000)
+    correctAnswer = x + y
+    print('what is', x, '+', y)
+
+    StudentAnswer = int(input('Please type the answer: '))
+
+
+    if correctAnswer == StudentAnswer:
+        print("Correct answer!")
+    else:
+        print("Wrong! The correct answer is ", correctAnswer)
+
+    #print(answer)
+
+if __name__ == '__main__':
+    mathProblem()
